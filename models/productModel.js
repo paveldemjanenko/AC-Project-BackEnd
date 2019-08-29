1bbc63b5291c706c3dfb5fd16e3bb252d68d4c52
Pavels Demjanenko <pavel.demjanenko@gmail.com>
2019-08-29 22:48:17 +0300

    Minor code beauty changes

--- models/productModel.js
+++ models/productModel.js
@@ -5,19 +5,15 @@ const productSchema = new mongoose.Schema(
     id: { type: Number, trim: true, unique: true, required: true },
     title: { type: String, trim: true, unique: false, required: true },
     media: {
-    //   contentId: { type: String, unique: true, required: true },
       path: { type: String, unique: true, required: true },
     },
     price: { type: Number, trim: true, unique: false, required: true },
     company: { type: String, trim: true, unique: false, required: true },
     info: { type: String, trim: true, unique: false, required: true },
-  },
-//   { timestamps: true },
+  }
 );
 
 const ProductModel = mongoose.model('Product', productSchema);
-
-// const save = async model => new ProductModel(model).save();
 
 const getProductById = async _id => ProductModel.findById({ _id });
 
